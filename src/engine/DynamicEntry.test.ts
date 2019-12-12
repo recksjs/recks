27990f7315fc9498bc325160d2738fbd051c7e97
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-12-12 17:47:48 +0200

    added basic DynamicEntry test

---
+++ src/engine/DynamicEntry.test.ts
@@ -0,0 +1,15 @@
+import { DynamicEntry } from './DynamicEntry';
+
+describe('DynamicEntry', () => {
+
+    test('Destroy', () => {
+        const dynamicEntry = DynamicEntry();
+        const mockObserver = {
+            complete: jest.fn()
+        };
+        dynamicEntry.result$.subscribe(mockObserver);
+        dynamicEntry.destroy$.next(void 0);
+
+        expect(mockObserver.complete.mock.calls.length).toBe(1);
+    });
+})
