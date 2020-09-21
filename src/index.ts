e53af5adc4612489577159fca3992d1e49b14c93
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-09-21 16:10:05 +0300

    adding TS support - attempt 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import { Observable } from 'rxjs';
 import { createElement } from './engine/Element';
 import { render } from './engine/render';
-import './types.d.ts';
 
 const Recks = { createElement, render };
+
+declare namespace Recks {
+    namespace JSX {
+        type LibraryManagedAttributes<
+            Component,
+            Props
+            > = Props extends Observable<infer O> ? O : Props;
+    }
+}
 
 export { Recks, createElement, render };
 export default Recks;
